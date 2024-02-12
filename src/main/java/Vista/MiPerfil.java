85b7ff73df681d9174c4cb7bbe6953429300c4d6
pedrorolgar <pedro.rolgar@educa.jcyl.es>
2024-02-12 07:00:01 +0100

    Mis Eventos
    Se ha creado la pantalla de mis eventos y se han erreglado errores

--- src/main/java/Vista/MiPerfil.java
+++ src/main/java/Vista/MiPerfil.java
@@ -29,8 +29,6 @@ public class MiPerfil extends javax.swing.JPanel {
 
     jLabel2 = new javax.swing.JLabel();
     jLabel1 = new javax.swing.JLabel();
-    EditarPerfilBtn = new javax.swing.JButton();
-    misEventosBtn = new javax.swing.JButton();
     jLabel3 = new javax.swing.JLabel();
     jLabel4 = new javax.swing.JLabel();
     jLabel5 = new javax.swing.JLabel();
@@ -38,6 +36,13 @@ public class MiPerfil extends javax.swing.JPanel {
     jLabel7 = new javax.swing.JLabel();
     jLabel8 = new javax.swing.JLabel();
     jLabel9 = new javax.swing.JLabel();
+    jLabel10 = new javax.swing.JLabel();
+    jLabel11 = new javax.swing.JLabel();
+    jLabel12 = new javax.swing.JLabel();
+    jLabel13 = new javax.swing.JLabel();
+    jLabel14 = new javax.swing.JLabel();
+    EditarPerfilBtn = new javax.swing.JButton();
+    misEventosBtn = new javax.swing.JButton();
 
     setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -46,18 +51,6 @@ public class MiPerfil extends javax.swing.JPanel {
 
     jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/PaisajeFondo.png"))); // NOI18N
     add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1145, 319));
-
-    EditarPerfilBtn.setBackground(new java.awt.Color(122, 137, 159));
-    EditarPerfilBtn.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
-    EditarPerfilBtn.setForeground(new java.awt.Color(255, 255, 255));
-    EditarPerfilBtn.setText("Editar Perfil");
-    add(EditarPerfilBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 354, 291, 42));
-
-    misEventosBtn.setBackground(new java.awt.Color(122, 137, 159));
-    misEventosBtn.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
-    misEventosBtn.setForeground(new java.awt.Color(255, 255, 255));
-    misEventosBtn.setText("Mis Eventos");
-    add(misEventosBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(842, 354, 291, 42));
 
     jLabel3.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 41)); // NOI18N
     jLabel3.setForeground(new java.awt.Color(255, 255, 255));
@@ -84,18 +77,50 @@ public class MiPerfil extends javax.swing.JPanel {
 
     jLabel8.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 27)); // NOI18N
     jLabel8.setForeground(new java.awt.Color(255, 255, 255));
-    jLabel8.setText("Ciudad");
-    add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(65, 567, -1, -1));
+    jLabel8.setText("Email");
+    add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(65, 659, 990, -1));
 
     jLabel9.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 27)); // NOI18N
     jLabel9.setForeground(new java.awt.Color(255, 255, 255));
     jLabel9.setText("Género");
     add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(65, 613, 970, -1));
+
+    jLabel10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconoLocalizacion.png"))); // NOI18N
+    add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(29, 568, 30, 30));
+
+    jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/mailPerfil.png"))); // NOI18N
+    add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(29, 662, 30, 30));
+
+    jLabel12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/genero.png"))); // NOI18N
+    add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(29, 613, 30, 30));
+
+    jLabel13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconoUsu.png"))); // NOI18N
+    add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 361, 24, 24));
+
+    jLabel14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/libreta.png"))); // NOI18N
+    add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(900, 365, 24, 24));
+
+    EditarPerfilBtn.setBackground(new java.awt.Color(122, 137, 159));
+    EditarPerfilBtn.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
+    EditarPerfilBtn.setForeground(new java.awt.Color(255, 255, 255));
+    EditarPerfilBtn.setText("Editar Perfil");
+    add(EditarPerfilBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 354, 291, 42));
+
+    misEventosBtn.setBackground(new java.awt.Color(122, 137, 159));
+    misEventosBtn.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 18)); // NOI18N
+    misEventosBtn.setForeground(new java.awt.Color(255, 255, 255));
+    misEventosBtn.setText("Mis Eventos");
+    add(misEventosBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(842, 354, 291, 42));
   }// </editor-fold>//GEN-END:initComponents
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JButton EditarPerfilBtn;
   private javax.swing.JLabel jLabel1;
+  private javax.swing.JLabel jLabel10;
+  private javax.swing.JLabel jLabel11;
+  private javax.swing.JLabel jLabel12;
+  private javax.swing.JLabel jLabel13;
+  private javax.swing.JLabel jLabel14;
   private javax.swing.JLabel jLabel2;
   private javax.swing.JLabel jLabel3;
   private javax.swing.JLabel jLabel4;
