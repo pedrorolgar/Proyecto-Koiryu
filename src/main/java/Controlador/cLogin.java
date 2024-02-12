bdc462172d76d801611f50e6597b5a43c3329763
pedrorolgar <pedro.rolgar@educa.jcyl.es>
2024-02-12 05:19:38 +0100

    Crear Evento
    Se ha creado el diseño parcial de la pantalla de crear diseño, se han añadido más herramientas que permiten al usuario tener una interfaz más dinámica y además se han corregido errores

--- src/main/java/Controlador/cLogin.java
+++ src/main/java/Controlador/cLogin.java
@@ -8,9 +8,9 @@ import Model.Usuarios;
 import Vista.Bienvenida;
 import Vista.Login;
 import com.formdev.flatlaf.extras.FlatSVGIcon;
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
 
 /**
  * The type C login.
@@ -19,73 +19,79 @@ import java.awt.event.MouseListener;
  */
 public class cLogin {
 
-    private final Login Vista;
+  private final Login Vista;
 
-    /**
-     * Instantiates a new C login.
-     *
-     * @param Vista the vista
-     */
-    public cLogin(Login Vista) {
-        this.Vista = Vista;
-    }
+  /**
+   * Instantiates a new C login.
+   *
+   * @param Vista the vista
+   */
+  public cLogin(Login Vista) {
+    this.Vista = Vista;
+  }
 
-    /**
-     * Actualizar estilos.
-     */
-    public void actualizarEstilos() {
-        Vista.getUsuLogin().putClientProperty("JTextField.placeholderText", "Nombre de usuario");
-        Vista.getContraLogin().putClientProperty("JTextField.placeholderText", "Contraseña");
-        Vista.getUsuLogin().putClientProperty("FlatLaf.style", "arc:10");
-        Vista.getContraLogin().putClientProperty("FlatLaf.style", "arc:10");
-        Vista.getUsuLogin().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/iconoUsuario.svg"));
-        Vista.getContraLogin().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/Password.svg"));
-    }
+  /**
+   * Actualizar estilos.
+   */
+  public void actualizarEstilos() {
+    Vista.getUsuLogin().putClientProperty("JTextField.placeholderText", "Nombre de usuario");
+    Vista.getContraLogin().putClientProperty("JTextField.placeholderText", "Contraseña");
+    Vista.getUsuLogin().putClientProperty("FlatLaf.style", "arc:10");
+    Vista.getContraLogin().putClientProperty("FlatLaf.style", "arc:10");
+    Vista.getUsuLogin().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/iconoUsuario.svg"));
+    Vista.getContraLogin().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/Password.svg"));
+  }
 
-    /**
-     * Inicializar eventos.
-     */
-    public void inicializarEventos() {
-        Vista.getLoginBtn().addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent mouseEvent) {
+  /**
+   * Inicializar eventos.
+   */
+  public void inicializarEventos() {
+    Vista.getLoginBtn().addMouseListener(new MouseListener() {
+      @Override
+      public void mouseClicked(MouseEvent mouseEvent) {
 
-            }
+      }
 
-            @Override
-            public void mousePressed(MouseEvent mouseEvent) {
+      @Override
+      public void mousePressed(MouseEvent mouseEvent) {
 
-            }
+      }
 
-            @Override
-            public void mouseReleased(MouseEvent mouseEvent) {
-                String nombre = Vista.getUsuLogin().getText();
-                String contrasena = String.valueOf(Vista.getContraLogin().getPassword());
-                Usuarios usuario = Usuarios.encontrarPorNombreDeUsuario(nombre);
-                if (usuario == null) {
-                    System.out.println("no se ha encontrado al usuario");
-                    return;
+      @Override
+      public void mouseReleased(MouseEvent mouseEvent) {
+        String nombre = Vista.getUsuLogin().getText();
+        String contrasena = String.valueOf(Vista.getContraLogin().getPassword());
+        Usuarios usuario = Usuarios.encontrarPorNombreDeUsuario(nombre);
+        if (usuario == null) {
+          System.out.println("no se ha encontrado al usuario");
+          JOptionPane.showMessageDialog(null, "El usuario no se ha encontrado o no existe", "Error", JOptionPane.WARNING_MESSAGE);
 
-                }
-                if (Usuarios.comprobarContraseña(contrasena, usuario)) {
-                    System.out.println("Funciona te has conectado " + nombre);
-                    new Bienvenida().setVisible(true);
-                    Vista.dispose();
-                } else {
-                    System.out.println("La contraseña no es correcta");
-                }
+          return;
 
-            }
+        }
+        if (Usuarios.comprobarContraseña(contrasena, usuario)) {
+          System.out.println("Funciona te has conectado " + nombre);
+          JOptionPane.showMessageDialog(null, "Te has conectado con éxito", "Bienvenido", JOptionPane.WARNING_MESSAGE);
 
-            @Override
-            public void mouseEntered(MouseEvent mouseEvent) {
+          new Bienvenida().setVisible(true);
+          Vista.dispose();
+        } else {
+          System.out.println("La contraseña no es correcta");
+          JOptionPane.showMessageDialog(null, "La contraseña no es correcta", "Error", JOptionPane.WARNING_MESSAGE);
 
-            }
+        }
 
-            @Override
-            public void mouseExited(MouseEvent mouseEvent) {
+      }
 
-            }
-        });
-    }
+      @Override
+      public void mouseEntered(MouseEvent mouseEvent) {
+
+      }
+
+      @Override
+      public void mouseExited(MouseEvent mouseEvent) {
+
+      }
+    });
+  }
 }
--- src/main/java/Controlador/cSignIn.java
+++ src/main/java/Controlador/cSignIn.java
@@ -10,6 +10,7 @@ import Vista.SignIn;
 import com.formdev.flatlaf.extras.FlatSVGIcon;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
 
 /**
  * The type C sign in.
@@ -18,78 +19,82 @@ import java.awt.event.MouseListener;
  */
 public class cSignIn {
 
-    private final SignIn Vista;
+  private final SignIn Vista;
 
-    /**
-     * Instantiates a new C sign in.
-     *
-     * @param Vista the vista
-     */
-    public cSignIn(SignIn Vista) {
-        this.Vista = Vista;
-    }
+  /**
+   * Instantiates a new C sign in.
+   *
+   * @param Vista the vista
+   */
+  public cSignIn(SignIn Vista) {
+    this.Vista = Vista;
+  }
 
-    /**
-     * Actualizar estilos.
-     */
-    public void actualizarEstilos() {
-        Vista.getUsuSign().putClientProperty("JTextField.placeholderText", "Nombre de usuario");
-        Vista.getCorreoSign().putClientProperty("JTextField.placeholderText", "TuCorreo@Gmail.com");
-        Vista.getContraSign().putClientProperty("JTextField.placeholderText", "Contraseña");
-        Vista.getUsuSign().putClientProperty("FlatLaf.style", "arc:10");
-        Vista.getContraSign().putClientProperty("FlatLaf.style", "arc:10");
-        Vista.getCorreoSign().putClientProperty("FlatLaf.style", "arc:10");
-        Vista.getGeneroCb().putClientProperty("FlatLaf.style", "arc:10");
-        Vista.getUsuSign().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/iconoUsuario.svg"));
-        Vista.getContraSign().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/Password.svg"));
-        Vista.getCorreoSign().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/Vector.svg"));
-    }
+  /**
+   * Actualizar estilos.
+   */
+  public void actualizarEstilos() {
+    Vista.getUsuSign().putClientProperty("JTextField.placeholderText", "Nombre de usuario");
+    Vista.getCorreoSign().putClientProperty("JTextField.placeholderText", "TuCorreo@Gmail.com");
+    Vista.getContraSign().putClientProperty("JTextField.placeholderText", "Contraseña");
+    Vista.getUsuSign().putClientProperty("FlatLaf.style", "arc:10");
+    Vista.getContraSign().putClientProperty("FlatLaf.style", "arc:10");
+    Vista.getCorreoSign().putClientProperty("FlatLaf.style", "arc:10");
+    Vista.getGeneroCb().putClientProperty("FlatLaf.style", "arc:10");
+    Vista.getUsuSign().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/iconoUsuario.svg"));
+    Vista.getContraSign().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/Password.svg"));
+    Vista.getCorreoSign().putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("imagenes/Vector.svg"));
+  }
 
-    /**
-     * Inicializar eventos.
-     */
-    public void InicializarEventos() {
-        Vista.getSignInBtn().addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                String nombreUsuario = Vista.getUsuSign().getText();
-                String contraseñaUsuario = String.valueOf(Vista.getContraSign().getPassword());
-                String correoUsuario = Vista.getCorreoSign().getText();
-                String generoUsuario = Vista.getGeneroCb().getSelectedItem().toString();
-                if (!(nombreUsuario.isEmpty() && correoUsuario.isEmpty() && generoUsuario.isEmpty() && contraseñaUsuario.isEmpty())) {
-                    Usuarios usuario = new Usuarios(nombreUsuario, correoUsuario, generoUsuario, contraseñaUsuario);
-                    System.out.println(usuario);
-                    if (Usuarios.guardarUsuarios(usuario)) {
-                        System.out.println("Se ha guardado el usuario");
-                        new Login().setVisible(true);
-                        Vista.dispose();
+  /**
+   * Inicializar eventos.
+   */
+  public void InicializarEventos() {
+    Vista.getSignInBtn().addMouseListener(new MouseListener() {
+      @Override
+      public void mouseClicked(MouseEvent e) {
+        String nombreUsuario = Vista.getUsuSign().getText();
+        String contraseñaUsuario = String.valueOf(Vista.getContraSign().getPassword());
+        String correoUsuario = Vista.getCorreoSign().getText();
+        String generoUsuario = Vista.getGeneroCb().getSelectedItem().toString();
+        if (!(nombreUsuario.isEmpty() && correoUsuario.isEmpty() && generoUsuario.isEmpty() && contraseñaUsuario.isEmpty())) {
+          Usuarios usuario = new Usuarios(nombreUsuario, correoUsuario, generoUsuario, contraseñaUsuario);
+          System.out.println(usuario);
+          if (Usuarios.guardarUsuarios(usuario)) {
+            System.out.println("Se ha guardado el usuario");
+            JOptionPane.showMessageDialog(null, "Usuario creado con éxito", "Bienvenido/a", JOptionPane.WARNING_MESSAGE);
 
-                    } else {
-                        System.out.println("No se ha guardado el usuario");
-                    }
+            new Login().setVisible(true);
+            Vista.dispose();
 
-                }
-            }
+          } else {
+            System.out.println("No se ha guardado el usuario");
+            JOptionPane.showMessageDialog(null, "No ha sido posible crear al usuario", "Error", JOptionPane.WARNING_MESSAGE);
 
-            @Override
-            public void mousePressed(MouseEvent e) {
+          }
 
-            }
+        }
+      }
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
+      @Override
+      public void mousePressed(MouseEvent e) {
 
-            }
+      }
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
+      @Override
+      public void mouseReleased(MouseEvent e) {
 
-            }
+      }
 
-            @Override
-            public void mouseExited(MouseEvent e) {
+      @Override
+      public void mouseEntered(MouseEvent e) {
 
-            }
-        });;
-    }
+      }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+
+      }
+    });;
+  }
 }
